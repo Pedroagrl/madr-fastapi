38c8f05891357bd073c22d81acec759d16f501f4
Pedro Artur <pedroartur25@gmail.com>
2025-05-07 17:15:16 -0300

    banco de dados com SQLAlchemy

--- madr/tests/conftest.py
+++ madr/tests/conftest.py
@@ -1,9 +1,22 @@
 import pytest
+from sqlalchemy import create_engine
 from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
 
+from madr.models import table_registry
 from madr.app import app
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+@pytest.fixture()
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+        
+    table_registry.metadata.drop_all(engine)
--- madr/tests/test_db.py
+++ madr/tests/test_db.py
@@ -1,22 +1,19 @@
-from madr.models import User, table_registry
-from sqlalchemy.orm import Session
+from madr.models import User
 
-from sqlalchemy import create_engine
+from sqlalchemy import select
 
-def test_create_user():
-    engine = create_engine('sqlite:///database.db')
-
-    table_registry.metadata.create_all(engine)
-
-    with Session (engine) as session:
-
-        user = User(username='Pedrao',
-                    email='pedro@gmail.com',
-                    password='123'
+def test_create_user(session):
+    user = User(
+        username='Pedrao',
+        email='pedro@gmail.com',
+        password='123'
         )
 
-        session.add(user)
-        session.commit()
-        session.refresh(user)
+    session.add(user)
+    session.commit()
 
-    assert user.id == 1
+    result = session.scalar(
+        select(User).where(User.email == 'pedro@gmail.com')
+    )
+
+    assert result.username == 'Pedrao'
